9ed529c18af7566d617a2fd483ae4f952792a143
MaryMaryVery <kalchenko.marya@gmail.com>
2022-06-27 19:42:49 +0300

    commit classwork

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const options = [
-    {value: 1, label: 'Audi'},
-    {value: 2, label: 'BMW'},
-    {value: 3, label: 'Citroen'},
-    {value: 4, label: 'Ford'},
-    {value: 5, label: 'Honda'},
-    {value: 6, label: 'Jaguar'},
-    {value: 7, label: 'Land Rover'},
-    {value: 8, label: 'Mini'},
-    {value: 9, label: 'Nissan'},
-    {value: 10, label: 'Toyota'},
-    {value: 11, label: 'Volvo'}
+    {value: 1, label: 'Audi', group: 'Германия'},
+    {value: 2, label: 'BMW', group: 'Германия'},
+    {value: 3, label: 'Citroen', group: 'Франция'},
+    {value: 4, label: 'Ford', group: 'США'},
+    {value: 5, label: 'Honda', group: 'Япония'},
+    {value: 6, label: 'Jaguar', group: 'Великобритания'},
+    {value: 7, label: 'Land Rover', group: 'Великобритания'},
+    {value: 8, label: 'Mini', group: 'Великобритания'},
+    {value: 9, label: 'Nissan', group: 'Япония'},
+    {value: 10, label: 'Toyota', group: 'Япония'},
+    {value: 11, label: 'Volvo', group: 'Швеция'}
 ]
 
 class DropDown {
@@ -32,10 +32,25 @@ class DropDown {
                 }
             }
         })
-        this.itemsHTML = this.options.map(({value, label}) => {
-            return `<li data-id=${value}>${label}</li>`
+
+        // this.itemsHTML = this.options.map(({value, label}) => {
+        //     return `<li data-id=${value}>${label}</li>`
+        // })
+
+        this.itemsHTML = this.initGroup(this.options).map(([key, items]) => {
+            if(key) {
+                const groupList = items.map(({value, label}) => `<li data-id=${value}>${label}</li>`).join('')
+                return `<li style="padding-left: 25px; color: gray"><span>${key}</span>${groupList}</li>`
+                }
+            else {
+                return items.map(({value, label}) => `<li data-id=${value}>${label}</li>`).join('')
+            }
+
         }).join('')
+
         this.$dropDown.insertAdjacentHTML("afterbegin", this.itemsHTML)
+
+
     }
     selectedItem(id) {
         this.$label.innerHTML = this.options.find(item => item.value === id).label
@@ -47,7 +62,21 @@ class DropDown {
     close() {
         this.$select.classList.remove('active')
     }
+
+    initGroup(items) {
+        const group = new Map()
+        items.forEach(item => {
+            if (!group.has(item.group)) {
+                group.set(item.group, [item])
+            } else {
+                group.set(item.group, [...group.get(item.group), item])
+            }
+        })
+        return Array.from(group.entries())
+
+    }
 }
+
 
 const customSelect = new DropDown('.select', options)
 
